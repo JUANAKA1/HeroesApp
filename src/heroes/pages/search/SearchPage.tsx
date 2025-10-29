f730957c8f5640b87ea5b34d774cbcc11c269417
JUANAKA1 <kmiloprieto73@gmail.com>
2025-10-29 07:31:05 -0500

    feat: add Radix UI components for improved search functionality and UI enhancements

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,23 @@ import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStarts } from "@/heroes/components/HeroStarts";
 import { SearchControls } from "./ui/SearchControls";
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
+import { searchHerosAction } from "@/heroes/actions/search-heros.action";
+
+import { HeroGrid } from "@/heroes/components/HeroGrid";
 
 const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get("name") ?? undefined;
+  const strength = searchParams.get("strength") ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ["search", { name, strength }],
+    queryFn: () => searchHerosAction({ name, strength }),
+    staleTime: 6000 * 5,
+  });
   return (
     <>
       <CustomJumbotron
@@ -12,14 +27,17 @@ const SearchPage = () => {
       />
       <CustomBreadcrumbs
         currentPage="Buscador de héroes"
-        breadcrumb={[
-          // { label: "Home", to: "/" },
-          // { label: "Home2", to: "/" },
-          // { label: "Home3", to: "/" },
-        ]}
+        breadcrumb={
+          [
+            // { label: "Home", to: "/" },
+            // { label: "Home2", to: "/" },
+            // { label: "Home3", to: "/" },
+          ]
+        }
       />
       <HeroStarts />
       <SearchControls />
+      <HeroGrid heroes={heroes} />
     </>
   );
 };
